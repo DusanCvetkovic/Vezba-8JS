2581bf0f3c1f295d2663be952d9852a105de3ed3
DusanCvetkovic <71986600+DusanCvetkovic@users.noreply.github.com>
2021-02-09 21:53:09 +0100

    Add files via upload

---
+++ Script8.js
@@ -0,0 +1,39 @@
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+
+
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByTagName('li'));
+console.log(document.getElementsByClassName('item'));
+
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
+
+
+const ul = document.querySelector('.items');
+
+    // ul.remove();
+    // ul.lastElementChild.remove();
+
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Dusan';
+ul.lastElementChild.innerHTML = '<h1>Good Bye</h1>';
+
+const btn = document.querySelector('.btn');
+btn.style.background = 'red';
+
+
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e.target.className);
+    document.getElementById('my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+  });
+  
+  
+  const nameInput = document.querySelector('#name');
+  nameInput.addEventListener('input', e => {
+    document.querySelector('.container').append(nameInput.value);
+  });
+  
